1e054b013d1841af64001acb24d854622f28d5df
Lynz <79261532+LynzChloe@users.noreply.github.com>
2022-02-10 12:49:54 +0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('USER_DOMAIN', 'demo.lynz.rocks');
+define('USER_DOMAIN', 'demo.lexir.rocks');
 # Url to redirect to when solving. Below is just an example, I recommend
 # putting a LinkVertise on top of it, so that way you can earn some money from ADs.
 define('USER_FINISH', 'https://' . USER_DOMAIN . '/Generate.php?solve=1');
